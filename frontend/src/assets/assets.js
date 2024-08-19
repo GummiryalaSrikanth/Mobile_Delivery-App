61562484bf2644221a6e5fab8182d016b0321ccd
Gummiryala Srikanth <gummiryalasrikanth369@gmail.com>
2024-08-19 21:14:09 +0530

    ReactJs

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,186 @@
+import basket_icon from './basket_icon.png'
+import logo from './logo.png'
+import header_img from './header_img.png'
+import search_icon from './search_icon.png'
+import menu_1 from './menu_1.png'
+import menu_2 from './menu_2.png'
+import menu_3 from './menu_3.png'
+import menu_4 from './menu_4.png'
+import menu_5 from './menu_5.png'
+import menu_6 from './menu_6.png'
+import menu_7 from './menu_7.png'
+import menu_8 from './menu_8.png'
+
+import phone_1 from './phone_1.png'
+import phone_2 from './phone_2.png'
+import phone_3 from './phone_3.png'
+import phone_4 from './phone_4.png'
+import phone_5 from './phone_5.png'
+import phone_6 from './phone_6.png'
+import phone_7 from './phone_7.png'
+import phone_8 from './phone_8.png'
+import phone_9 from './phone_9.png'
+import phone_10 from './phone_10.png'
+import phone_11 from './phone_11.png'
+import phone_12 from './phone_12.png'
+
+import add_icon_white from './add_icon_white.png'
+import add_icon_green from './add_icon_green.png'
+import remove_icon_red from './remove_icon_red.png'
+import app_store from './app_store.png'
+import play_store from './play_store.png'
+import linkedin_icon from './linkedin_icon.png'
+import facebook_icon from './facebook_icon.png'
+import twitter_icon from './twitter_icon.png'
+import cross_icon from './cross_icon.png'
+import selector_icon from './selector_icon.png'
+import rating_starts from './rating_starts.png'
+import profile_icon from './profile_icon.png'
+import bag_icon from './bag_icon.png'
+import logout_icon from './logout_icon.png'
+import parcel_icon from './parcel_icon.png'
+
+export const assets = {
+    logo,
+    basket_icon,
+    header_img,
+    search_icon,
+    rating_starts,
+    add_icon_green,
+    add_icon_white,
+    remove_icon_red,
+    app_store,
+    play_store,
+    linkedin_icon,
+    facebook_icon,
+    twitter_icon,
+    cross_icon,
+    selector_icon,
+    profile_icon,
+    logout_icon,
+    bag_icon,
+    parcel_icon
+}
+
+export const menu_list = [
+    {
+        menu_name: "iPhone",
+        menu_image: menu_1
+    },
+    {
+        menu_name: "OnePlus",
+        menu_image: menu_2
+    },
+    {
+        menu_name: "Xiaomi",
+        menu_image: menu_3
+    },
+    {
+        menu_name: "Samsung",
+        menu_image: menu_4
+    },
+    {
+        menu_name: "Oppo",
+        menu_image: menu_5
+    },
+    {
+        menu_name: "Vivo",
+        menu_image: menu_6
+    },
+    {
+        menu_name: "Realme",
+        menu_image: menu_7
+    },
+    {
+        menu_name: "Nokia",
+        menu_image: menu_8
+    }]
+
+export const food_list = [
+    {
+        _id: "1",
+        name: "iPhone 15",
+        image: phone_1,
+        price: 12,
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, qui!",
+        category: "iPhone"
+    },
+    {
+        _id: "2",
+        name: "iPhone 14",
+        image: phone_2,
+        price: 18,
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, qui!",
+        category: "iPhone"
+    }, {
+        _id: "3",
+        name: "iPhone 13",
+        image: phone_3,
+        price: 16,
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, qui!",
+        category: "iPhone"
+    }, {
+        _id: "4",
+        name: "iPhone 12",
+        image: phone_4,
+        price: 24,
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, qui!",
+        category: "iPhone"
+    }, {
+        _id: "5",
+        name: "OnePlus-14",
+        image: phone_5,
+        price: 14,
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, qui!",
+        category: "OnePlus"
+    }, {
+        _id: "6",
+        name: "OnePlus-12",
+        image: phone_6,
+        price: 12,
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, qui!",
+        category: "OnePlus"
+    }, {
+        _id: "7",
+        name: "oneplus nord ce 2 lite",
+        image: phone_7,
+        price: 20,
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, qui!",
+        category: "OnePlus"
+    }, {
+        _id: "8",
+        name: "oneplus nord 4",
+        image: phone_8,
+        price: 15,
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, qui!",
+        category: "OnePlus"
+    }, {
+        _id: "9",
+        name: "Redmi 11",
+        image:phone_9,
+        price: 14,
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, qui!",
+        category: "Xiaomi"
+    }, {
+        _id: "10",
+        name: "Redmi 10",
+        image: phone_10,
+        price: 22,
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, qui!",
+        category: "Xiaomi"
+    }, {
+        _id: "11",
+        name: "Redmi 9",
+        image: phone_11,
+        price: 10,
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, qui!",
+        category: "Xiaomi"
+    }, {
+        _id: "12",
+        name: "Redmi 8",
+        image: phone_12,
+        price: 12,
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, qui!",
+        category: "Xiaomi"
+    }
+]
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Header.css";
+
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order your favourite mobile here.</h2>
+        <p>
+          Discover our latest collection of mobile phones, featuring
+          cutting-edge technology, sleek designs, and unbeatable performance.
+          From top brands like Apple, Samsung, and Google, find the perfect
+          device to match your lifestyle. Whether you need the latest camera
+          features, powerful processors, or long-lasting battery life, our
+          selection has something for everyone. Shop now and stay connected in
+          style.
+        </p>
+        <button>
+         
+            View Menu
+          
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
